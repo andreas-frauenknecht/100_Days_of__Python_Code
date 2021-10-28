daa274f23942d8e46f8d5f968051d07af7cda17b
Andreas Frauenknecht <andreas.frauenknecht@gmail.com>
2021-10-28 16:12:17 +0200

    End of day 1

---
+++ Day 1/Day1_Project_BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator program\n")
+city = input("What's the name of the City you grew up in? ")
+pet_name = input("What's your pet's name? ")
+print("Your band name could be "+city+" "+pet_name)
--- Day 1/Day1_printing_start.py
+++ Day 1/Day1_printing_start.py
@@ -1,5 +1,24 @@
+#My work file for day one of the 100 Days of code curse
+
 print("Hello World")
 
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print('what to print')")
+print("print('what to print')\n")
+
+print("Or you could do it like this:\n\nDay 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')\n")
+
+print("Hello " + "Angela\n")
+
+print("Hello " + input ("What is your name?\n"))
+
+print(len(input("What is your name? ")))
+
+name = input("What is your name? ")
+print(name)
+name = "But now your name is Jack"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
