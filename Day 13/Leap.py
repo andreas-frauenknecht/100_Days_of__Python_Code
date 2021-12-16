3246d659145a43c11039529b8d34ab4f9d56210b
Andreas Frauenknecht <andreas.frauenknecht@gmail.com>
2021-12-16 20:56:05 +0100

    day 14 done

---
+++ Day 13/Leap.py
@@ -0,0 +1,13 @@
+year = int(input("Which year do you want to check?"))  #bugix, not converted to ing
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+  
